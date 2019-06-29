6d8a015b14878355635a7fb0975a3807fc48d54f
thomaslaforgia <thomaslaforgia@gmail.com>
2019-06-29 18:23:32 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ApplicationRecord
+  validates :artist_name, presence: true
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name]
+  }
+  validates :released, inclusion: { in: [ true, false] }
+  validates :artist_name, presence: true
+  with_options if: :released? do |s|
+    s.validates :release_year, presence: true
+    s.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
+
+end
